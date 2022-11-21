15ef7d6ebda674dbca226a2ca883a9e462263787
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-21 17:49:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include "TelCoColurCoder.hpp"
 
+TelCoColorCoder :: ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+  }
+
 ColorPair::MajorColor getMajor() {
     return majorColor;
  }
 ColorPair:: MinorColor getMinor() {
       return minorColor;
  }
-ColorPair::std::string ToString() {
+ std::string ToString() {
   std::string colorPairStr = MajorColorNames[majorColor];
   colorPairStr += " ";
   colorPairStr += MinorColorNames[minorColor];
