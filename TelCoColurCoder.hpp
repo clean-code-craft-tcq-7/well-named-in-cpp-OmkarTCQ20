a4c372cbaaf00febff6a33f218acd03d37e17b36
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-22 11:51:12 +0530

    Update TelCoColurCoder.hpp

--- TelCoColurCoder.hpp
+++ TelCoColurCoder.hpp
@@ -29,21 +29,30 @@ namespace TelCoColorCoder
             ColorPair(MajorColor major, MinorColor minor):
                 majorColor(major), minorColor(minor)
             {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            std::string ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
     };
-    
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor);
-    
- void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber);
-    
- ColorPair GetColorFromPairNumber(int pairNumber);
- int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-    
-  MajorColor getMajor();
-  MinorColor getMinor();
-  }   
-#endif
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
+#endif 
