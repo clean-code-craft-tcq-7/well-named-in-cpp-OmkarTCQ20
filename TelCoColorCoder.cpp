999ff0b2e77a03ef6768f475ef84bab1f50d4415
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-22 15:34:45 +0530

    Update TelCoColorCoder.cpp

--- TelCoColorCoder.cpp
+++ TelCoColorCoder.cpp
@@ -1,13 +1,27 @@
 #include "TelCoColurCoder.hpp"
 
-void testNumberToPair();
+void testNumberToPair(int pairNumber, MajorColor expectedMajor,MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(MajorColor major,MinorColor minor,int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
 
 int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+    
+    testNumberToPair(4, WHITE,BROWN);
+    testNumberToPair(5, WHITE,SLATE);
 
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+    testPairToNumber(BLACK, TORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
 
     return 0;
 }
