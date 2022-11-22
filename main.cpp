762545c7ee4a092d9aed0b8d0834734d7b3113f3
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-22 09:34:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ TelCoColorCoder :: ColorPair GetColorFromPairNumber(int pairNumber) {
          (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
       return ColorPair(majorColor, minorColor);
  }
-TelCoColorCoder ::  int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+int TelCoColorCoder :: GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
  }
 
@@ -26,3 +26,24 @@ TelCoColorCoder :: ColorPair:: MajorColor getMajor() {
 TelCoColorCoder :: ColorPair:: MinorColor getMinor() {
       return minorColor;
  }
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber)
+{
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
